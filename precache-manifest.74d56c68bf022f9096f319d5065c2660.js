03b771ec69cbe3c6718a0adbd089c32df673315f
darrylgit <darrylsmithup@gmail.com>
2019-06-29 16:40:36 -0400

    Updates

---
+++ precache-manifest.74d56c68bf022f9096f319d5065c2660.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46a3fac83227a6951701",
+    "url": "/todo-calendar-project/static/css/main.63469de5.chunk.css"
+  },
+  {
+    "revision": "46a3fac83227a6951701",
+    "url": "/todo-calendar-project/static/js/main.fef71a16.chunk.js"
+  },
+  {
+    "revision": "c1f02fdc15bb9925c330",
+    "url": "/todo-calendar-project/static/js/runtime~main.54be615d.js"
+  },
+  {
+    "revision": "ceaa6a73d6e7c9f54ed3",
+    "url": "/todo-calendar-project/static/js/2.9a22511a.chunk.js"
+  },
+  {
+    "revision": "3ea3baf0abf189b4d2282ee9b670bc3a",
+    "url": "/todo-calendar-project/index.html"
+  }
+];
